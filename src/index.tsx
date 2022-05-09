4818750ed35dd114b0cc184a76440eb533b7434c
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-09 13:43:12 +0300

    updated react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import {Provider} from "react-redux";
-import AppWithRedux from "./AppWithRedux";
 import {store} from "./state/store";
 
 const root = ReactDOM.createRoot(
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>
-)
+);
 
 
 
