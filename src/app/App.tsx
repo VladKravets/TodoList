2ccafe1457fd5c557e71d9401a19760526d24b81
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-18 11:31:43 +0300

    fixed project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,105 +1,25 @@
-import React, {useCallback, useEffect} from 'react'
-import './App.css';
-import {Todolist} from '../featuries/Todolist/Todolist';
-import {AddItemForm} from '../components/Input/AddItemForm';
-import {
-    AppBar,
-    Button,
-    Container,
-    Grid,
-    IconButton,
-    LinearProgress,
-    Paper, Snackbar,
-    Toolbar,
-    Typography
-} from '@material-ui/core';
-import {Menu} from '@material-ui/icons';
-import {
-    addTodolistTC,
-    changeTodolistFilterAC,
-    changeTodolistTitleTC, deleteTodolistTC, fetchTodolistsTC, FilterValuesType,
-    TodolistDomainType,
-} from '../featuries/Todolist/todolists-reducer';
-import {
-    addTaskTC,
-    changeTaskTitleAC,
-    deleteTaskTC, updateTaskStatusTC,
-} from '../featuries/Todolist/Task/tasks-reducer';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppRootStateType} from './store';
-import {TaskStatuses, TaskType} from "../API/API";
-import {RequestStatusType} from "./app-reducer";
+import React from 'react'
+import './App.css'
+import { TodolistsList } from '../features/TodolistsList/TodolistsList'
 
+// You can learn about the difference by reading this guide on minimizing bundle size.
+// https://mui.com/guides/minimizing-bundle-size/
+// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Menu } from '@mui/icons-material';
+import {ErrorSnackbar} from "../components/SnackBar";
 
-export type TasksStateType = {
-    [key: string]: Array<TaskType>
-}
-
-/*
-const Fake = React.memo(function() {
-    console.log("FAKE")
-    const arr = useSelector<AppRootStateType, Array<TaskType>>(state => state.tasks.count)
-    return <h1>{arr.length}</h1>
-})
-*/
 
 function App() {
 
-    useEffect(() => {
-        //@ts-ignore
-        dispatch(fetchTodolistsTC())
-    }, []);
-
-    const todolists = useSelector<AppRootStateType, Array<TodolistDomainType>>(state => state.todolists)
-    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
-    const dispatch = useDispatch();
-
-
-    const removeTask = useCallback(function (tasksId: string, todolistId: string) {
-        //@ts-ignore
-        dispatch(deleteTaskTC(todolistId, tasksId));
-    }, []);
-
-    const addTask = useCallback(function (title: string, todolistId: string,) {
-        const thunk = (addTaskTC(title, todolistId));
-        //@ts-ignore
-        dispatch(thunk)
-    }, []);
-
-    const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
-        //@ts-ignore
-        dispatch(updateTaskStatusTC(taskId, todolistId, status));
-    }, []);
-
-    const changeTaskTitle = useCallback(function (id: string, newTitle: string, todolistId: string) {
-        const action = changeTaskTitleAC(id, newTitle, todolistId);
-        dispatch(action);
-    }, [dispatch]);
-
-    const changeFilter = useCallback(function (value: FilterValuesType, todolistId: string) {
-        const action = changeTodolistFilterAC(todolistId, value);
-        dispatch(action);
-    }, [dispatch]);
-
-    const addTodolist = useCallback((title: string) => {
-        //@ts-ignore
-        dispatch(addTodolistTC(title));
-    }, []);
-
-    const removeTodolist = useCallback(function (todolistId: string) {
-        //@ts-ignore
-        dispatch(deleteTodolistTC(todolistId));
-    }, []);
-
-    const changeTodolistTitle = useCallback((todolistId: string, title: string) => {
-        //@ts-ignore
-        dispatch(changeTodolistTitleTC(todolistId, title));
-    }, [])
-
-    const status = useSelector<AppRootStateType,RequestStatusType>(state => state.app.status)
     return (
         <div className="App">
-            <Snackbar/>
+            <ErrorSnackbar/>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
@@ -111,40 +31,11 @@ function App() {
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
-            {status === 'loading' && <LinearProgress color="secondary"/>}
             <Container fixed>
-                <Grid container style={{padding: "20px"}}>
-                    <AddItemForm addItem={addTodolist}/>
-                </Grid>
-                <Grid container spacing={3}>
-                    {
-                        todolists.map(tl => {
-                            let allTodolistTasks = tasks[tl.id];
-                            let tasksForTodolist = allTodolistTasks;
-
-                            return <Grid item key={tl.id}>
-                                <Paper style={{padding: "10px"}}>
-                                    <Todolist
-                                        id={tl.id}
-                                        title={tl.title}
-                                        tasks={tasksForTodolist}
-                                        removeTask={removeTask}
-                                        changeFilter={changeFilter}
-                                        addTask={addTask}
-                                        changeTaskStatus={changeStatus}
-                                        filter={tl.filter}
-                                        removeTodolist={removeTodolist}
-                                        changeTaskTitle={changeTaskTitle}
-                                        changeTodolistTitle={changeTodolistTitle}
-                                    />
-                                </Paper>
-                            </Grid>
-                        })
-                    }
-                </Grid>
+                <TodolistsList/>
             </Container>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
