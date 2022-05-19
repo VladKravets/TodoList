009ce3e905d2bbaf575c9c62a00534333839d186
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-19 14:24:07 +0300

    added user authorization

---
+++ src/api/auth-api.ts
@@ -0,0 +1,22 @@
+import axios, {AxiosResponse} from "axios";
+import {ResponseType} from "./todolists-api";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '3bb1f301-4343-478c-bd6d-e432168f19d7'
+    }
+})
+export type LoginParamsType = {
+    email: string,
+    password: string,
+    rememberMe: boolean
+    captcha?: string
+}
+export const authAPI = {
+    login(data: LoginParamsType) {
+        const promise = instance.post<LoginParamsType, AxiosResponse<ResponseType<{ userId?: number }>>>('auth/login', data)
+        return promise
+    }
+}
